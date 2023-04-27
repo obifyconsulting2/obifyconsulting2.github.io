b762aca717411e1809c735dc71cdcf09b016b0f2
Ranjan <skillsapphire@gmail.com>
2023-04-27 15:35:50 +0200

    added logic to read api

--- ecommerce/main.js
+++ ecommerce/main.js
@@ -1,1 +1,7 @@
-alert("Welcome!");
+async function loadProducts() {
+    let res = await fetch('https://obifyconsulting2.github.io/json/products.json');
+    let products = await res.json();
+    console.log(products);
+}
+
+loadProducts();
