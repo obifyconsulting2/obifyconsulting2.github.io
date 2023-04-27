018e9d08863eb4d45ad316bbd035854443db9622
Ranjan <skillsapphire@gmail.com>
2023-04-27 15:59:37 +0200

    changes

--- ecommerce/main.js
+++ ecommerce/main.js
@@ -2,8 +2,8 @@ async function loadProducts() {
     let res = await fetch('https://obifyconsulting2.github.io/json/products.json');
     let products = await res.json();
     let markup = "<ul>";
-    for (let i = 0; i < products.length; i++) {
-        markup = markup + `<li>${products[i].title}</li>`
+    for (let i = 0; i < products.list.length; i++) {
+        markup = markup + `<li>${products.list[i].title}</li>`
     }
     markup = markup + "</ul>";
     document.querySelector("#products").innerHTML = markup;
